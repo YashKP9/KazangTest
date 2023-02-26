470540161090c7bdcb94ec25c24a0002cc353992
Yash <ypillay@masthead.co.za>
2023-02-27 01:37:36 +0200

    Question 10: Used a Pre-Operation plugin before commit to DB on SubEntity, to maintain data integrity.  Only 1 unauthorized subentity is permitted.  Class Library (Shared) would be more optimized between workflow and plugin assemblies in future.

---
+++ Kazang/Plugins/Common/SubEntityManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace Kazang.Plugins
+{
+    public class SubEntityManager
+    {
+        private Utils UtilManager = new Utils();
+
+        public SubEntityManager() { }
+
+        public EntityCollection RetrieveSubEntities(Guid EntityGUID, IOrganizationService CrmService)
+        {
+            String RetLog = String.Empty;
+            EntityCollection entityCollection = null;
+            try
+            {
+                String FetchXML = String.Empty;
+                FetchXML = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='true'>
+                                                <entity name='new_subentity'>
+                                                <attribute name='new_name' />
+                                                <attribute name='new_subentityid' />
+                                                <attribute name='new_authorize' />
+                                                <order attribute='createdon' descending='true' />
+                                                <filter type='and'>  
+                                                   <condition attribute='new_master' operator='eq' value='" + EntityGUID + @"' />     
+                                                   <condition attribute='statecode' operator='eq' value='0' />                                         
+                                                </filter>
+                                                </entity>
+                                            </fetch>";
+
+                entityCollection = UtilManager.ExecuteFetch(CrmService, FetchXML);
+                if (entityCollection != null)
+                {
+                    RetLog = RetLog + entityCollection.Entities.Count;
+                }
+                else
+                {
+                    RetLog = RetLog + "No SubEntities Found";
+                }
+            }
+            catch (Exception ex)
+            {
+                entityCollection = null;
+                RetLog = RetLog + ex.Message;
+            }
+            return entityCollection;
+        }
+
+    }
+}
---
+++ Kazang/Plugins/SubEntityPreCreate.cs
@@ -0,0 +1,84 @@
+﻿// <copyright file="SubEntityPreCreate.cs" company="">
+// </copyright>
+// <author>YASH PILLAY</author>
+// <date>27/2/2023 1:22:00 AM</date>
+// <summary>Implements the SubEntityPreCreate Plugin.</summary>
+namespace Kazang.Plugins
+{
+    using System;
+    using System.ServiceModel;
+    using Microsoft.Xrm.Sdk;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Data;
+    using System.Configuration;
+
+    /// <summary>
+    /// SubEntityPreCreate Plugin.
+    /// </summary>    
+    public class SubEntityPreCreate : Plugin
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SubEntityPreCreate"/> class.
+        /// </summary>
+        public SubEntityPreCreate()
+            : base(typeof(SubEntityPreCreate))
+        {
+            base.RegisteredEvents.Add(new Tuple<int, string, string, Action<LocalPluginContext>>(20, "Create", "new_subentity", new Action<LocalPluginContext>(ExecuteSubEntityPreCreate)));
+
+            // Note : you can register for more events here if this plugin is not specific to an individual entity and message combination.
+            // You may also need to update your RegisterFile.crmregister plug-in registration file to reflect any change.
+        }
+
+        /// <summary>
+        /// Executes the plug-in. IN ORDER TO MAINTAIN DATA INTEGRITY, PREVENT COMMIT TO DB ON PRE-EVENT, 
+        /// SO THAT ONLY 1 UNAUTHORIZED RECORD IS ATTACHED TO MASTER
+        /// </summary>
+        /// <param name="localContext">The <see cref="LocalPluginContext"/> which contains the
+        /// <see cref="IPluginExecutionContext"/>,
+        /// <see cref="IOrganizationService"/>
+        /// and <see cref="ITracingService"/>
+        /// </param>
+        protected void ExecuteSubEntityPreCreate(LocalPluginContext localContext)
+        {
+            if (localContext == null)
+            {
+                throw new ArgumentNullException("localContext");
+            }
+           
+
+            IPluginExecutionContext context = localContext.PluginExecutionContext;
+            IOrganizationService _service = localContext.OrganizationService;
+
+            Entity entity = (Entity)context.InputParameters["Target"];
+
+            String RetLog = String.Empty;
+            // The InputParameters collection contains all the data passed in the message request.
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                if (entity.LogicalName == "new_subentity")
+                {
+                    SubEntityManager sem = new SubEntityManager();
+                    bool currUnAuth = entity.Attributes.Contains("new_authorize") && (entity["new_authorize"] == null || (bool)entity["new_authorize"] == false) ? true : (!entity.Attributes.Contains("new_authorize") ? true: false);
+                    if (entity.Attributes.Contains("new_master") && currUnAuth)
+                    {
+                        Guid guidMaster = ((EntityReference)entity["new_master"]).Id;
+                        EntityCollection ecSubEnts = sem.RetrieveSubEntities(guidMaster, _service);
+                        foreach (Entity sub in ecSubEnts.Entities)
+                        {
+                            if (sub.Attributes.Contains("new_authorize"))
+                            {
+                                if (sub["new_authorize"] == null || (bool)sub["new_authorize"] == false)
+                                {
+                                    throw new InvalidPluginExecutionException("Not Permitted: Only 1 Unauthorized Sub Entity may exist at a given time.");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
